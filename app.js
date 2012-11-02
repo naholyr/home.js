ef026172d3c0d6ddc26ac8975d7bbd022c2a1954
Nicolas Steinmetz <nsteinmetz@gmail.com>
2012-11-02 22:40:04 +0100

    Some progress but need to understand/manage/kill callbacks to make it work

--- app.js
+++ app.js
@@ -20,23 +20,24 @@ swig.init({
     allowErrors: true // allows errors to be thrown and caught by express instead of suppressed
 });
 
-
-
-
 app.get('/', function (req, res) {
-	function displayTitle (article) {
-	  var arttitle = JSON.stringify(article.title)
-	  console.log('Got title: %s', arttitle);
+  var feeds = { content: []};
+	function displayTitle (error, meta, articles) {
+	  articles.forEach(function (article){
+      // console.log('%s - %s (%s)', article.date, article.title, article.link);
+      feeds.content.push ({"date":article.date, "title": article.title,  "link": article.link});
+    });
 	}
 	async.parallel ([
-		function() { feedparser.parseUrl('http://archives.steinmetz.fr/journal/feeds/all.atom.xml').on('article', displayTitle) },
-		function() { feedparser.parseUrl('http://nsteinmetz.tumblr.com/rss').on('article', displayTitle) },
+		function(cb) { feedparser.parseUrl('http://archives.steinmetz.fr/journal/feeds/all.atom.xml', displayTitle); cb(null, feeds)},
+		/*function() { feedparser.parseUrl('http://nsteinmetz.tumblr.com/rss').on('article', displayTitle) },
 		function() { feedparser.parseUrl('http://archives.steinmetz.fr/tutoriels/feeds/all.atom.xml').on('article', displayTitle) },
 		function() { feedparser.parseUrl('http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=nsteinmetz').on('article', displayTitle)  },
+    */
 	], function(err, results) {
 		res.render('index', {
                   foo: 'bar',
-                  feeds: [] // TODO calculate from results
+                  feeds: JSON.parse(results) // TODO calculate from results
                 });
 	});
 });
